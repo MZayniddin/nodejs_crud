044e6418a9cb15a9c0244219eeba4de620291a7d
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-02-18 18:27:09 +0500

    created put method

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const http = require("http");
 const { read_file, write_file } = require("./fs-functions/fs");
 const productsFileName = "products.json";
-const options = { "content-type": "aplication/json" };
+const options = {
+  "content-type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+};
 
 const server = http.createServer((req, res) => {
+  const productId = +req.url.split("/")[3];
+
   if (req.method === "GET") {
     if (req.url === "/product/list") {
       res.writeHead(200, options);
@@ -24,7 +29,34 @@ const server = http.createServer((req, res) => {
         });
         write_file(productsFileName, products);
         res.writeHead(201, options);
-        res.end(JSON.stringify({ msg: "Successfully created"}))
+        res.end(JSON.stringify({ msg: "Successfully created" }));
+      });
+    }
+  }
+
+  if (req.method === "PUT") {
+    console.log("keldi")
+    if (req.url === `/product/update/${productId}`) {
+      req.on("data", (newData) => {
+        const { title, price } = JSON.parse(newData);
+        const productsArr = read_file(productsFileName);
+        let fileChanged = false;
+
+        productsArr.forEach((product) => {
+          if (product.id === productId) {
+            product.title = title ? title : product.title;
+            product.price = price ? price : product.price;
+            fileChanged = true;
+            res.writeHead(200, options);
+            res.end(JSON.stringify({ msg: "Successfully updated!" }));
+          }
+        });
+
+        write_file(productsFileName, productsArr);
+        if (!fileChanged) {
+          res.writeHead(404, options);
+          res.end(JSON.stringify({ msg: "Product not founded" }));
+        }
       });
     }
   }
