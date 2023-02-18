8f2a6255b3df380c7534044776e2f68b9a7136e7
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-02-18 10:35:52 +0500

    initialized server

---
+++ fs/fs.js
@@ -0,0 +1,5 @@
+// const fs = require("fs");
+
+// const readFile = (fileName) => {
+//   return JSON.parse(fs.readFileSync(`./models/${fileName}`));
+// };
---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  
+})
+
+server.listen(() => {
+  console.log("server is running on 3000 port")
+})
